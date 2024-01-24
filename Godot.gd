59ecbe1c0e6017e8453ac4a047ce273cae1bf8fc
MagicalAnt <157605453+MagicalAnt@users.noreply.github.com>
2024-01-24 23:53:18 +0800

    Update Godot.gd

--- Godot.gd
+++ Godot.gd
@@ -1,15 +1,23 @@
 extends Area2D
 
+var random = RandomNumberGenerator.new()
+#var randX = random.randi_range(20,100)
+#var randY = random.randi_range(20,100)
+
 var score: int = 0
 var MAX_SCORE = 10
 
 signal win
 signal lost
 
+func _ready():
+	_rand_position()
+
 func _input_event(viewport, event, shape_idx):
 	#print(event)
 	if (event.is_action_pressed("click")):
 		score = score +1
+		_rand_position()
 		print(score)
 		if (score == MAX_SCORE):
 			self.hide()
@@ -31,3 +39,10 @@ func _on_timer_timeout():
 	emit_signal("lost")
 	self.hide()
 #	pass # Replace with function body.
+
+func _rand_position():
+	random.randomize()
+	var randX = random.randi_range(20,get_viewport_rect().size.x -20)
+	var randY = random.randi_range(20,get_viewport_rect().size.y - 20)
+	self.set_global_position(Vector2(randX, randY))
+	
